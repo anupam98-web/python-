5ac5d76bcb4f079ce51207846a4c1778e72b6f8c
anupam98-web <65861544+anupam98-web@users.noreply.github.com>
2020-09-05 11:54:22 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,19 +1,10 @@
 from django import forms
 
-class Exlmapping(forms.Form):
-        Job_ID = forms.CharField()
-        Company_Name = forms.CharField()
-        Job_Category = forms.CharField()
-        Job_Sub_Category = forms.CharField()
-        HR_Name = forms.CharField()
-        Job_Role = forms.CharField()
-        Skills_Required = forms.CharField()
-        Creation_Date = forms.CharField()
-        End_Date = forms.CharField()
-        Number_of_openings = forms.CharField()
-        Jopb_Location = forms.CharField()
-        Designation_of_job = forms.CharField()
-        ctc = forms.CharField()
-        Min_Exp = forms.CharField()
-        Max_Exp = forms.CharField()
-        CompanyID = forms.CharField()
+from .models import Mandate
+
+class MandateForm(forms.ModelForm):
+
+    class Meta:
+        model = Mandate
+        fields = '__all__'
+
